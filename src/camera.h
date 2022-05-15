e653e7cad8c74ae2031c5240b4161001e5ce9f61
kenshin627 <87zhaoxiaobo@163.com>
2022-05-15 23:53:55 +0800

    material

--- src/camera.h
+++ src/camera.h
@@ -5,20 +5,26 @@
 
 class camera {
 public:
-	camera() {
-		auto aspect_ratio = 16.0 / 9.0;
-		auto viewport_height = 2.0;
-		auto viewport_width = aspect_ratio * viewport_height;
-		auto focal_length = 1.0;
-
-		origin = point3(0, 0, 0);
-		horizontal = vec3(viewport_width, 0.0, 0.0);
-		vertical = vec3(0.0, viewport_height, 0.0);
-		lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	camera() { }
+	camera(const point3& from, const vec3& lookAt, const vec3& up, const double fov, const double aspect_ratio, const double focal, double aperture) {
+		origin = from;
+		auto z = unit_vector(origin - lookAt);
+		auto x = unit_vector(cross(up, z));
+		auto y = unit_vector(cross(z, x));
+		double fovRadian = degrees_to_radians(fov);
+		double viewport_H = tan(fovRadian / 2) *  2;
+		double viewport_W = aspect_ratio * viewport_H;
+		horizontal = viewport_W * x * focal;
+		vertical = viewport_H * y * focal;
+		lower_left_corner = origin - horizontal / 2 - vertical / 2 - z * focal;
+		lens_radius = aperture / 2;
 	}
 
 	ray get_ray(double u, double v) const {
-		return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+
+		vec3 rd = lens_radius * random_in_unit_disk();
+		vec3 offset = x * rd.x() + y * rd.y();
+		return ray(origin + offset, lower_left_corner + u * horizontal + v * vertical - origin - offset);
 	}
 
 private:
@@ -26,6 +32,8 @@ private:
 	vec3 lower_left_corner;
 	vec3 horizontal;
 	vec3 vertical;
+	vec3 x, y, z;
+	double lens_radius;
 };
 #endif // !CAMERA_H
 
--- src/material.h
+++ src/material.h
@@ -30,14 +30,46 @@ public:
 
 class metal : public material {
 public:
-	metal(const color3& a) : albedo(a) {}
+	metal(const color3& a, double f) : albedo(a), fuzz(f < 1? f: 1) {}
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color3& attenuation, ray& scattered) const override {
 		auto scatter_direction = reflect(unit_vector(r_in.direction()), rec.normal);
-		scattered = ray(rec.p, scatter_direction);
+		scattered = ray(rec.p, scatter_direction + fuzz * random_unitVec() );
 		attenuation = albedo;
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
 public:
 	color3 albedo;
+	double fuzz;
 };
+
+class dietectirc : public material {
+public:
+	dietectirc(double index_of_refractio) : ir(index_of_refractio) {}
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color3& attenuation, ray& scattered) const override {
+		double refraction_radio = rec.front_face ? 1.0 / ir : ir;
+		auto cos_theta = fmin(dot(-unit_vector(r_in.direction()), rec.normal), 1.0);
+		auto sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+		vec3 scatter_direction;
+		bool totalInternalReflect = sin_theta * refraction_radio > 1.0;
+		if (totalInternalReflect || reflectance(cos_theta, refraction_radio) > random_double())
+		{
+			scatter_direction = reflect(unit_vector(r_in.direction()), rec.normal);
+		}
+		else {
+			scatter_direction = refract(unit_vector(r_in.direction()), rec.normal, refraction_radio);
+		}
+		attenuation = color3(1.0, 1.0, 1.0);
+		scattered = ray(rec.p, scatter_direction);
+		return true;
+	}
+public:
+	double ir;
+private:
+	static double reflectance(double cosine, double ref_idx) {
+		auto r0 = (1 - ref_idx) / (1 + ref_idx);
+		r0 = r0 * r0;
+		return r0 + (1 - r0) * pow((1 - cosine), 5);
+	}
+};
+
 #endif // !MATERIAL_H
